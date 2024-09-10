3f14ddc423ea657d8b9228c482abcf178aaba032
Zeroject <47432671+zeroject@users.noreply.github.com>
2024-09-10 11:56:44 +0200

    Update BookingManagerTests.cs

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -66,5 +66,67 @@ namespace HotelBooking.UnitTests
             Assert.Empty(bookingForReturnedRoomId);
         }
 
+        [Fact]
+        public void Create_A_New_Booking()
+        {
+            // Arrange
+            Booking booking = new Booking();
+            DateTime date = DateTime.Today.AddDays(1);
+            booking.RoomId = bookingManager.FindAvailableRoom(date, date);
+            booking.StartDate = date;
+            booking.EndDate = date;
+
+            // ACT
+            bool succes = bookingManager.CreateBooking(booking);
+
+            // Assert
+            Assert.True(succes);
+        }
+
+        [Theory]
+        [InlineData(1, typeof(ArgumentException))] // Days extra is positive, expecting ArgumentException
+        [InlineData(0, null)] // Days extra is negative, no exception expected
+        [InlineData(5000, typeof(ArgumentException))] // High number of extra days, expecting ArgumentException
+        [InlineData(4, typeof(ArgumentException))] // A specific number of extra days, expecting ArgumentException
+        public void FindAvailableRoom(int daysextra, Type expectedException)
+        {
+            // Arrange
+            DateTime date = DateTime.Today.AddDays(1);
+
+            // Act
+            Action act = () => bookingManager.FindAvailableRoom(date.AddDays(daysextra), date);
+
+            if (expectedException != null)
+            {
+                // Assert exception
+                Assert.Throws(expectedException, act);
+            }
+            else
+            {
+                // Assert no exception
+                var exception = Record.Exception(act);
+                Assert.Null(exception);  // No exception should be thrown for this case
+            }
+        }
+
+
+        [Theory]
+        [InlineData(1, 10, 1)]
+        [InlineData(1, 20, 11)]
+        [InlineData(1, 1, 0)]
+        [InlineData(-30, 0, 0)]
+        [InlineData(1, 5, 0)]
+        public void GetFullyOccupiedDates_Tests(int startOffsetDays, int endOffsetDays, int expected)
+        {
+            // Arrange
+            DateTime startDate = DateTime.Today.AddDays(startOffsetDays);
+            DateTime endDate = DateTime.Today.AddDays(endOffsetDays);
+
+            // Act
+            var result = bookingManager.GetFullyOccupiedDates(startDate, endDate);
+
+            // Assert
+            Assert.Equal(expected, result.Count); // Assert that the number of fully occupied days matches the expected value
+        }
     }
 }
